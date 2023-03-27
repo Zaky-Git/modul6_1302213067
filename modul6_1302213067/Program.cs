f1d398af2629e07d0d46662316b8ca3e4a4fdf9c
Zaky <mzakyf5503@gmail.com>
2023-03-27 08:33:41 +0700

    penambahan design by contract pada semua kelas

--- modul6_1302213067/Program.cs
+++ modul6_1302213067/Program.cs
@@ -9,8 +9,18 @@ internal class Program
         Jepri.AddVideo(Video1);
         sayaTubeVideo Video2 = new sayaTubeVideo("BrandI got Kicked");
         sayaTubeVideo Video3 = new sayaTubeVideo("BrandE got Kicked");
+        sayaTubeVideo Video4 = new sayaTubeVideo("Brand1E got Kicked");
+        sayaTubeVideo Video5 = new sayaTubeVideo("BrandE got Kicked");
+        sayaTubeVideo Video6 = new sayaTubeVideo("Brand3E got Kicked");
+        sayaTubeVideo Video7 = new sayaTubeVideo("BrandE2 got Kicked");
+        sayaTubeVideo Video8 = new sayaTubeVideo("Brand45E got Kicked");
         Jepri.AddVideo(Video2);
         Jepri.AddVideo(Video3);
+        Jepri.AddVideo(Video4);
+        Jepri.AddVideo(Video5);
+        Jepri.AddVideo(Video6);
+        Jepri.AddVideo(Video7);
+        Jepri.AddVideo(Video8);
 
         Jepri.PrintAllVideoPlaycount();
 
