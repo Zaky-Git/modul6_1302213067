98a964f43f7e33897bf008cbf8ae2c1d5cedd3be
Zaky <mzakyf5503@gmail.com>
2023-03-27 07:46:16 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul6_1302213067/sayaTubeUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace modul6_1302213067
+{
+    internal class sayaTubeUser
+    {
+        private int id;
+        private List<sayaTubeVideo> uploadVideos;
+        public string Username;
+
+        public sayaTubeUser(string username)
+        {
+            this.Username = username;
+            Random random = new Random();
+            this.id = random.Next(9999, 99999);
+            List<sayaTubeUser> uploadVideos= new List<sayaTubeUser>();
+        }
+
+
+
+        public int getTotalVideoPlayCount()
+        {
+            int tot = 0;
+            for (int i = 0;i<uploadVideos.Count;i++)
+            {
+                tot = tot + uploadVideos[i].getPlayCount();
+            }
+            return tot;
+        }
+
+        public void AddVideo(sayaTubeVideo video)
+        {
+            uploadVideos.Add(video);
+        }
+
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("User " + Username);
+            for (int i = 0;i<uploadVideos.Count;i++)
+            {
+                Console.WriteLine("Video " + (i+1) + " Judul " + uploadVideos[i]);
+            }
+        }
+    }}
---
+++ modul6_1302213067/sayaTubeVideo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302213067
+{
+    internal class sayaTubeVideo
+    {
+        int id;
+        string title;
+        int playCount;
+        public sayaTubeVideo(string title)
+        {
+            this.title = title;
+            int playCount = 0;
+            Random rand = new Random();
+            this.id = rand.Next(9999, 99999);
+        }
+
+        public int getPlayCount()
+        {
+            return playCount;
+        }
+
+        public void increasePlayCount(int count)
+        {
+            playCount = count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video id " + id + " Judul " +
+                title + " diputar sebanyak " + playCount + " kali");
+        }
+    }
+}
